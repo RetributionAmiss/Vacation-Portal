96bc0219ca76b4292c019e1ca52356d4e3af1c72
RetributionAmiss <159307372+RetributionAmiss@users.noreply.github.com>
2026-08-16 14:40:22 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.VACATION_PORTAL_CONFIG = {
   // Paste the current deployed Apps Script /exec URL here before hosting.
-  portalUrl: 'PASTE_CURRENT_APPS_SCRIPT_EXEC_URL_HERE',
+  portalUrl: 'https://script.google.com/macros/s/AKfycbwkb5g56znjd3MEJUa-HTZbzAIOdG4g9oL5TbF4_wuhcTIOYceDd_yJ3RWgt384xJq8vw/exec',
 
   appName: 'Family Vacation Portal',
   shortName: 'Family Trip',
