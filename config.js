03e4df84c393902c55d7b3e1a80b1a91c14ce17c
RetributionAmiss <159307372+RetributionAmiss@users.noreply.github.com>
2026-08-25 14:03:06 -0400

    Point Alpha1 PWA test shell to test Apps Script deployment

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 window.VACATION_PORTAL_CONFIG = {
-  // Paste the current deployed Apps Script /exec URL here before hosting.
-  portalUrl: 'https://script.google.com/macros/s/AKfycbwkb5g56znjd3MEJUa-HTZbzAIOdG4g9oL5TbF4_wuhcTIOYceDd_yJ3RWgt384xJq8vw/exec',
+  // Alpha1 test Apps Script deployment. Production config remains untouched.
+  portalUrl: 'https://script.google.com/macros/s/AKfycbwC8UNR26OeFIcqsybYvCpLcf9CfTud57-uasb6NC8F7CbYdCVNe0I8EaP18BLb1st-/exec',
 
   appName: 'Family Vacation Portal',
   shortName: 'Family Trip',
   oneSignalAppId: 'fac4d46d-ba5b-4755-b5b1-8740f59c3b1d',
-  release: 'V4.3.30'
+  release: 'V4.4.0-alpha1-test'
 };
