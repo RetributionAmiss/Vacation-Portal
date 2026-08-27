39af4b80451d72c8afa2baa05c183782890d7b04
RetributionAmiss <159307372+RetributionAmiss@users.noreply.github.com>
2026-08-27 12:56:01 -0400

    Point GitHub Pages shell to working Apps Script backend

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 window.VACATION_PORTAL_CONFIG = {
-  // Current production Apps Script deployment.
-  portalUrl: 'https://script.google.com/macros/s/AKfycbwkb5g56znjd3MEJUa-HTZbzAIOdG4g9oL5TbF4_wuhcTIOYceDd_yJ3RWgt384xJq8vw/exec',
+  // Internal Apps Script backend. Travelers should always use the GitHub Pages URL.
+  portalUrl: 'https://script.google.com/macros/s/AKfycbyvMM80eSg_6AijaQU6iVJFjk6B5cwzjOvd3-Kunp7lmIkNzBIrbRdg9dtqTF40mF3V0w/exec',
 
   appName: 'Family Vacation Portal',
   shortName: 'Family Trip',
   oneSignalAppId: 'fac4d46d-ba5b-4755-b5b1-8740f59c3b1d',
-  release: 'V4.4.0-alpha1'
+  release: 'V4.4.0-alpha2'
 };
