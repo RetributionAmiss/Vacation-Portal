0227cc2c2a78d6e926e6d786cdf9e7cc6764a65f
RetributionAmiss <159307372+RetributionAmiss@users.noreply.github.com>
2026-08-27 14:26:51 -0400

    Rotate Apps Script backend to fresh web deployment (#7)

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.VACATION_PORTAL_CONFIG = {
   // Internal Apps Script backend. Travelers should always use the GitHub Pages URL.
-  portalUrl: 'https://script.google.com/macros/s/AKfycbyvMM80eSg_6AijaQU6iVJFjk6B5cwzjOvd3-Kunp7lmIkNzBIrbRdg9dtqTF40mF3V0w/exec',
+  portalUrl: 'https://script.google.com/macros/s/AKfycbxH2RA7kV1L0-zQ8OdA58u10mUxF0WbQh3WnzyPrQ3IvcoODNdkbQA82X1rS4Q_8JPSnw/exec',
 
   appName: 'Family Vacation Portal',
   shortName: 'Family Trip',
