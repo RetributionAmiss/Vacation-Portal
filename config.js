994241170e5ef1c79a91b4e3feb8372873a29308
RetributionAmiss <159307372+RetributionAmiss@users.noreply.github.com>
2026-09-11 16:40:04 -0400

    Point alpha2.28 preview at isolated Apps Script backend

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.VACATION_PORTAL_CONFIG = {
   // Internal Apps Script backend. Travelers should always use the GitHub Pages URL.
-  portalUrl: 'https://script.google.com/macros/s/AKfycbzfo6ZXGGxQFApb6h0XTUmTKdeCLtSvTjoGXR1mKIuv1laEjZnREhHyJ01wguFfRxWOpA/exec',
+  portalUrl: 'https://script.google.com/macros/s/AKfycby_FG5Bdl9Vt8stKlx-ywfr8QlhXDB4xaKv7upBw-dKRP_C9nLs1FEeOfNOvoQbgmgWRg/exec',
 
   appName: 'Family Vacation Portal',
   shortName: 'Family Trip',
